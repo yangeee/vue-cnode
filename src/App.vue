35aa3f792660d908ef9bef6cdf3ee41c25063e33
yangeee <534650291@qq.com>
2019-04-08 17:19:54 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header"
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+}
+</script>
+  
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,67 @@
+<template>
+  <nav>
+    <div class="header">
+      <img src="https://static2.cnodejs.org/public/images/cnodejs_light.svg" alt>
+      <ul>
+        <li>
+          <a href="#">首页</a>
+        </li>
+        <li>
+          <a href="#">新手入门</a>
+        </li>
+        <li>
+          <a href="#">API</a>
+        </li>
+        <li>
+          <a href="#">关于</a>
+        </li>
+        <li>
+          <a href="#">注册</a>
+        </li>
+        <li>
+          <a href="#">登陆</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+
+<style scoped>
+* {
+  font-size: 13px;
+}
+a {
+  text-decoration: none;
+  color: #ccc;
+}
+img {
+  max-width: 120px;
+}
+ul {
+  display: flex;
+  list-style: none;
+  justify-content: center;
+  align-items: center;
+}
+ul li {
+  margin: 5px 15px;
+}
+nav {
+  background: #444444;
+  padding: 10px 90px;
+}
+.header{
+  display: flex;
+  justify-content: space-between;
+
+  align-items: center;
+}
+</style>
